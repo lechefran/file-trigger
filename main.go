029cdd3fb6c89c875137d55507a5f4a27e3db53b
lechefran <francis.sy98@gmail.com>
2024-12-20 00:13:42 -0600

    add additional azure blob functions

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ import (
 )
 
 func main() {
-	createBlobClient("REPLACE_WITH_AZURE_BLOB_STORAGE_ACCOUNT_URL")
+	client := createBlobClient("REPLACE_WITH_AZURE_BLOB_STORAGE_ACCOUNT_URL")
+	createBlobContainer(client, "sample-blob-container")
 }
 
+/*
+AZURE BLOB STORAGE FUNCTIONS
+*/
 func createBlobClient(url string) *azblob.Client {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
@@ -41,6 +45,52 @@ func createBlobContainer(client *azblob.Client, name string) bool {
 	return res
 }
 
+func downloadBlob(client *azblob.Client, containerName, filename, path string) {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = client.DownloadFile(context.TODO(), containerName, filename, file, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func downloadToByteArr(client *azblob.Client, containerName, filename string) []byte {
+	var barr []byte
+	_, err := client.DownloadBuffer(context.TODO(), containerName, filename, barr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return barr
+}
+
+func uploadBlob(client *azblob.Client, containerName, filename, path string) {
+	file, err := os.OpenFile(path+"/"+filename, os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}(file)
+
+	_, err = client.UploadFile(context.TODO(), containerName, filename, file, nil)
+}
+
+func uploadFromByteArr(client *azblob.Client, containerName, filename string, barr []byte) {
+	_, err := client.UploadBuffer(context.TODO(), containerName, filename, barr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+FILE FUNCTIONS
+*/
 func readFile(file string) []byte {
 	f, err := os.Open(file)
 	if err != nil {
